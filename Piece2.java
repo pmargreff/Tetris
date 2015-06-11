1d305efd040a881074e04b3868c714dbcd33eff2
Pablo Margreff <pablo_margreff@hotmail.com>
2015-06-11 20:42:36 -0300

    Implementa o método move

--- Piece2.java
+++ Piece2.java
@@ -1,6 +1,6 @@
+
 import java.awt.Point;
 import java.util.ArrayList;
-
 
 /**
  *
@@ -8,9 +8,13 @@ import java.util.ArrayList;
  */
 public class Piece2 extends Piece {
 
+    int flag_;
+
     Piece2(int width, int height) {
         super(width, height);
         int j = 1;
+
+        flag_ = 0;
 
         for (int i = 0; i < 4; i++) {
 
@@ -25,5 +29,84 @@ public class Piece2 extends Piece {
                 j++;
             }
         }
+    }
+
+    /**
+     * Turn the piece in opposite clockwise direction
+     * Set one vector for 'x' and one vector for 'y'
+     * and change the flag
+     */
+    
+    public void Turn() {
+        Point ref = new Point(body_.get(1)); //reference point
+        Point tmp = new Point(ref); 
+        int x[] = new int[3];
+        int y[] = new int[3];
+        
+        switch (flag_) {
+
+            case 0:
+                x[0] = -1;
+                y[0] = -1;
+                
+                x[1] = -1;
+                y[1] = 1;
+                
+                x[2] = -2;
+                y[2] = 2;
+                
+                flag_ = 1;
+                break;
+            case 1:
+                x[0] = 1;
+                y[0] = -1;
+                
+                x[1] = -1;
+                y[1] = -1;
+                
+                x[2] = -2;
+                y[2] = -2;
+                
+                flag_ = 2;
+                break;
+
+            case 2:
+                x[0] = 1;
+                y[0] = 1;
+                
+                x[1] = 1;
+                y[1] = -1;
+                
+                x[2] = 2;
+                y[2] = -2;
+                
+                flag_ = 3;
+                break;
+
+            case 3:
+                x[0] = -1;
+                y[0] = 1;
+                
+                x[1] = 1;
+                y[1] = 1;
+                
+                x[2] = 2;
+                y[2] = 2;
+                
+                flag_ = 0;
+                break;
+
+        }
+
+        //reference 'ref' to calculate and  set the new cell of piece
+        tmp.setLocation((ref.getX() + x[0]), (ref.getY() - y[0]));
+        body_.set(0, tmp);
+
+        tmp.setLocation((ref.getX() + x[1]), (ref.getY() - y[1]));
+        body_.set(2, tmp);
+
+        tmp.setLocation((ref.getX() + x[2]), (ref.getY() - y[2]));
+        body_.set(3, tmp);
+
     }
 }

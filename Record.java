40d802f9f9fa1ac1d35dda4a38c786d090605d7a
Pablo Margreff <pablo_margreff@hotmail.com>
2015-07-03 03:42:30 -0300

    Flag deleted. Record class documented

--- Record.java
+++ Record.java
@@ -4,90 +4,86 @@
  * and open the template in the editor.
  */
 
-
 //http://www.mballem.com/post/manipulando-arquivo-txt-com-java/
 //http://www.guj.com.br/articles/13
-
 import javax.swing.*;
 import java.io.*;
 
 /**
- *
- * @author gilberto
+ * Class use to get or save the record
  */
 public class Record {
-    
+
     private File file_ = new File("record.txt");
     private int record_;
-    
-    Record(){
-            try{
+
+    /**
+     * Try open the record file
+     */
+    Record() {
+        try {
             file_.createNewFile();
-            }catch(IOException ex){
-                
-            }
+        } catch (IOException ex) {
+
+        }
     }
-    
-    public int getRecord(){
+
+    /**
+     * Get the record
+     *
+     * @return Return the record
+     */
+    public int getRecord() {
         readRecord();
-        
-        return record_; 
+
+        return record_;
     }
-    
-    public void setRecord(int score){
+
+    /**
+     * Set the new record and save in file
+     *
+     * @param score The score that will be save
+     */
+    public void setRecord(int score) {
         record_ = score;
-        
+
         writeRecord();
     }
-    
+
     //https://www.youtube.com/watch?v=qqeOwdPZDvk
-    
-    private void writeRecord(){
-        
-        try{
+    /**
+     * Write the record in the file
+     */
+    private void writeRecord() {
+
+        try {
             FileWriter fileWriter = new FileWriter(file_, false);
-            
+
             try (PrintWriter printWriter = new PrintWriter(fileWriter)) {
                 printWriter.println(record_);
-                
+
                 printWriter.flush();
             }
-        }catch(IOException ex){
-            
+        } catch (IOException ex) {
+
         }
-        
-        
-        
+
     }
-    
-    private void readRecord(){
-        
-        try{
+
+    private void readRecord() {
+
+        try {
             FileReader fileReader = new FileReader(file_);
-            
+
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            
+
             String linha;
-            
-            linha = bufferedReader.readLine();  
-            
+
+            linha = bufferedReader.readLine();
+
             record_ = Integer.parseInt(linha);
-        }catch(IOException ex){
-            
+        } catch (IOException ex) {
+
         }
     }
-    
-    public static void main(String[] argv){
-        
-        Record record = new Record();
-        
-        int score = 30;
-        
-        record.setRecord(score);
-        
-        System.out.println(record.getRecord());
-
 }
-}
-
-
